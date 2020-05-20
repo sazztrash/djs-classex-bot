8625c5ce0c8606f04e24c5144b4378e599ecb1a4
Sazzo <stolensoul32@gmail.com>
2020-05-20 02:08:26 -0300

    wip: added a wip giveaway command (with bugs and not API implementation/winner result)

--- src/commands/Giveaway.js
+++ src/commands/Giveaway.js
@@ -1,16 +1,41 @@
 const { Command } = require('../structures/Command')
 
-module.exports = class Ping extends Command {
+module.exports = class Giveaway extends Command {
   constructor (client) {
     super(client, {
       name: 'giveaway',
       aliases: null,
-      requiredPermissions: null,
-      dev: true
+      requiredPermissions: 'manageGuild',
+      dev: false
     })
   }
   
   async run({ message, args }) {
-    this.client.createMessage(message.channel.id, 'owo')
+  let title = args.slice(0).join(" ")
+  let time = args.slice(1).join(" ")
+  if(!title.startsWith("title")) return this.client.createMessage(message.channel.id, 'You did not specify the title.')
+  // if(!time.startsWith("duration")) return this.client.createMessage(message.channel.id, 'You did not specify the duration.')
+  let formatedTitle = title.replace("title ", "")
+  let formatedTime = time.replace("duration ", "")
+  // plz help me
+  formatedTime = formatedTime.replace(formatedTitle, "")
+  formatedTime = formatedTime.replace(args[1], "")
+  formatedTitle = formatedTitle.replace(formatedTime, "")
+  formatedTitle = formatedTitle.replace("duration", "")
+  if(!formatedTime.endsWith("h") && !formatedTime.endsWith("m")) return this.client.createMessage(message.channel.id, 'This does not seem like a valid duration! Remember, we only accept durations in hours or minutes (example: 1h, 1m)')
+  const embedMessage = await this.client.createMessage(message.channel.id, {
+    embed: {
+      title: formatedTitle,
+      description: `Ends in ${formatedTime}`,
+      author: { 
+        name: message.author.username,
+        icon_url: message.author.avatarURL
+    },
+   footer: { 
+      text: "Created with .giveaway"
+  }
+    }
+  })
+  this.client.addMessageReaction(message.channel.id, embedMessage.id, '🎉')
   }
 }
