b6bb3476ee8a8f4ee5cc3ced16531730ba1d2d3a
Sazzo <stolensoul32@gmail.com>
2020-05-21 02:44:31 -0300

    Test Checks

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { DotGiveaway } = require('./src/dotgiveaway')
 const Sentry = require('@sentry/node')
 require('dotenv').config()
+// owo
 Sentry.init({ dsn: process.env.SENTRY_DSN })
 console.log('Starting.')
 const client = new DotGiveaway(process.env.DISCORD_TOKEN, {
