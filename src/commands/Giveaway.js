5c5a364638d616c14f49647128712afcb8d69918
Sazzo <stolensoul32@gmail.com>
2020-05-22 16:56:24 -0300

    feat: add giveaway command

--- src/commands/Giveaway.js
+++ src/commands/Giveaway.js
@@ -15,35 +15,33 @@ module.exports = class Giveaway extends Command {
   }
 
   async run ({ message, args }) {
-   const duration = args[0]
-   const winnerCount = args[1]
-   const title = args.slice(2).join(' ')
-   let users = []
-   
-   const format = await this.timerHandler.get(ms(duration))
-   console.log(format)
-   const embedMessage = await this.client.createMessage(message.channel.id, {
-     embed: {
-       title: title,
-       footer: {
-         text: `${winnerCount} winner(s)`
-       },
-       description: `${Messages.invite}\n${format}`,
-       color: 0x008000,
-       timestamp: this.timerHandler.count(ms(duration))
-     }
-   })
-   message.channel.addMessageReaction(embedMessage.id, '🎉')
-   setTimeout(async () => {
-     const reactions = await message.channel.getMessageReaction(embedMessage.id, '🎉')
-     reactions.forEach(user => {
-       if(user.bot) return
-       users.push(user)
-     });
-     const winner = await users[Math.floor(Math.random() * users.length)]
-     this.client.createMessage(message.channel.id, Messages.winnerMessage.replace('{winners}', `<@${winner.id}>`).replace('{prize}', title))
-     
-     
-   }, ms(duration));
+    const duration = args[0]
+    const winnerCount = args[1]
+    const title = args.slice(2).join(' ')
+    const users = []
+
+    const format = await this.timerHandler.get(ms(duration))
+    console.log(format)
+    const embedMessage = await this.client.createMessage(message.channel.id, {
+      embed: {
+        title: title,
+        footer: {
+          text: `${winnerCount} winner(s)`
+        },
+        description: `${Messages.invite}\n${format}`,
+        color: 0x008000,
+        timestamp: this.timerHandler.count(ms(duration))
+      }
+    })
+    message.channel.addMessageReaction(embedMessage.id, '🎉')
+    setTimeout(async () => {
+      const reactions = await message.channel.getMessageReaction(embedMessage.id, '🎉')
+      reactions.forEach(user => {
+        if (user.bot) return
+        users.push(user)
+      })
+      const winner = await users[Math.floor(Math.random() * users.length)]
+      this.client.createMessage(message.channel.id, Messages.winnerMessage.replace('{winners}', `<@${winner.id}>`).replace('{prize}', title))
+    }, ms(duration))
   }
 }
--- src/util/Messages.js
+++ src/util/Messages.js
@@ -1,11 +1,11 @@
 module.exports = {
-    winnerMessage: 'Congratulations, {winners}! You won **{prize}**!',
-    invite: 'React with 🎉 to participate!',
-    time: 'Time remaining: **{duration}**!',
-    units: {
-        seconds: "seconds",
-        minutes: "minutes",
-        hours: "hours",
-        days: "days",
-    }
+  winnerMessage: 'Congratulations, {winners}! You won **{prize}**!',
+  invite: 'React with 🎉 to participate!',
+  time: 'Time remaining: **{duration}**!',
+  units: {
+    seconds: 'seconds',
+    minutes: 'minutes',
+    hours: 'hours',
+    days: 'days'
+  }
 }
